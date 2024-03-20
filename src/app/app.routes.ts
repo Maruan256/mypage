0a70c2f0ced3fcbde54023358aa3fb75df27dd67
Maruan Paschen <maruanpaschen@gmail.com>
2024-03-20 18:48:27 +0100

    coming soon hinzugefuegt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { CvComponent } from './cv/cv.component';
 import { CssBattleComponent } from './css-battle/css-battle.component';
 import { GamesSiteComponent } from './games-site/games-site.component';
 import { SpaceinvadersComponent } from './games/spaceinvaders/spaceinvaders.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 
 export const routes: Routes = [
   { path: 'anschreiben', component: AnschreibenComponent},
@@ -14,6 +15,6 @@ export const routes: Routes = [
   { path: 'cv', component: CvComponent },
   { path: 'css-battle', component: CssBattleComponent },
   { path: 'games-site', component: GamesSiteComponent},
-  { path: 'games/spaceinvaders', component: SpaceinvadersComponent}
-
+  { path: 'games/spaceinvaders', component: SpaceinvadersComponent},
+  { path: 'coming-soon', component: ComingSoonComponent}
 ];
